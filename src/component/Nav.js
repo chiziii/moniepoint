83e26d3dd0b233f86fd2fe3dd8d0d78075df8d67
chizuruIgwe <igwechizy@gmail.com>
2024-02-26 01:35:40 +0100

    animation update

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,26 +1,56 @@
 import React from 'react';
+import { useSpring, animated } from '@react-spring/web'
 import logo from '../img/moni-logo.png'
 
 function Navbar() {
+  const springs = useSpring({
+    from: { y: 100 },
+    to: { y: 0 },
+  })
+
+  const navItems = useSpring({
+    from: { y: 50 },
+    to: { y: 0 },
+  })
+
     return (
       <div className='App'>
+         <animated.div
+      style={{  ...springs }}>
     <nav className="navbar">
     <div className="navbar-list">
             <img className="logo" src={logo} alt='logo' />
             <div className="list-wrapper">
         <span>           
-          <a className="navbar-item" href="/">Dashboard</a>
+        <animated.div
+      style={{ ...navItems }} className="navbar-item">
+        <a className="navbar-item" href="/">Dashboard</a>
+        </animated.div>
+        <animated.div
+      style={{  ...navItems }} className="navbar-item">
           <a className="navbar-item" href="/about">Reports</a>
+          </animated.div>
+          <animated.div
+      style={{  ...navItems }} className="navbar-item">
           <a className="navbar-item" href="/services">Documents</a>
+          </animated.div>
+          <animated.div
+      style={{  ...navItems }} className="navbar-item">
           <a className="navbar-item" href="/contact">History</a>
+          </animated.div>
+          <animated.div
+      style={{  ...navItems }} className="navbar-item">
           <a className="navbar-item" href="/contact">Settings</a>
+          </animated.div>
               </span> 
               </div>
-            <button className="sign-up">
+              <animated.button
+      style={{  ...navItems }} className="sign-up">
               Sign Up
-            </button>   
+            </animated.button>
       </div>
     </nav>
+  </animated.div>
     </div>
   );
 }
